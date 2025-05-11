c5a9d18df6c4d5f0471cfe4db5a0ba7ae1e27110
Shi Keming <1269736459@qq.com>
2025-05-11 21:35:39 +0800

    inp cy 2

--- vsrc/top.v
+++ vsrc/top.v
@@ -1,6 +1,7 @@
 `ifdef SIMULATION
 import "DPI-C" function void check_finsih(int y);
 `endif
+
 module top(
     input clk,
     input rst,
@@ -8,11 +9,9 @@ module top(
     input [11:0]b,
     input [11:0]c,
     input e,
-    output [12:0] y,
-    output [11:0] d
+    output [12:0] y
 );
 reg [11:0] d_reg;
-assign d = d_reg;
 reg [3:0]count;
 wire init_end = (count == 4'd12) ? 1'b1 : 1'b0;
 
@@ -43,48 +42,38 @@ reg [11:0] Fir_a_reg;//a
 reg [11:0] Fir_b_reg;//b
 
 Adder12 Adder12 (
-    .x_in      (a    ),
-    .y_in      (d_reg    ),
+    .x_in      (a           ),
+    .y_in      (d_reg       ),
     .result_out(Fir_add_wire)
 );
 
 Rom Rom(
     .clk       (clk          ),
     .rst       (rst          ),
-    .x_in       	(c        ),
-    .res_out 	    (Sec_rom_wire    )
+    .x_in      (c            ),
+    .res_out   (Sec_rom_wire )//两周期
 );
 
 always @(posedge clk) begin
     if (rst) begin
         Sec_rom_reg <= 0;
+        Fir_add_reg <= 0;
         Fir_a_reg <= 0;
         Fir_b_reg <= 0;
     end else begin
         Sec_rom_reg <= Sec_rom_wire;
+        Fir_add_reg <= Fir_add_wire;
         Fir_a_reg <= a;
         Fir_b_reg <= b;
     end
 end
-
-
-
-
-always @(posedge clk) begin
-    if (rst) begin
-        Fir_add_reg <= 0;
-    end
-    else  begin
-        Fir_add_reg <= Fir_add_wire;
-    end
-end//a+d得到结果
 
 reg  Fir_sign_reg;
 always @(posedge clk) begin
     if (rst) begin
         Fir_sign_reg <= 0;
     end else begin
-        Fir_sign_reg <= Sec_rom_reg[11];
+        Fir_sign_reg <= Sec_rom_wire[11];
     end
 end
 
@@ -107,28 +96,28 @@ reg  [3:0]  Sec_sft_reg;
 
 reg  [11:0] Sec_a_reg;//a
 
-reg [10:0] add_in;
+reg [10:0] div_in;
 reg [1:0] sftsel;
 always @(*)begin
     if (Fir_add_reg[12]) begin
-        add_in = Fir_add_reg[12:2];
+        div_in = Fir_add_reg[12:2];
         sftsel = 2'b10;
     end
     else if (Fir_add_reg[11]) begin
-        add_in = Fir_add_reg[11:1];
+        div_in = Fir_add_reg[11:1];
         sftsel = 2'b01;
     end
     else begin
-        add_in = Fir_add_reg[10:0];
+        div_in = Fir_add_reg[10:0];
         sftsel = 2'b00;
     end
 end
 
 DivRom DivRom(
-    .sel(sftsel),
-    .in  (add_in        ),
-    .sft (Sec_sft_wire       ),//Q1.12
-    .div (Sec_div_wire            )
+    .sel (sftsel        ),
+    .in  (div_in        ),
+    .sft (Sec_sft_wire  ),//Q1.12
+    .div (Sec_div_wire  )
 );
 
 
@@ -184,18 +173,18 @@ wire [23:0] Thi_bcos_wire;//(b*cos c)>>12
 reg [11:0] Thi_bcos_reg;//(b*cos c)>>12
 
 Wallace12x12 Wallace12x12_2 (
-    .clk        (clk          ),
-    .rst        (rst          ),
-    .x_in       (Sec_a_reg             ),
-    .y_in       (Sec_div_reg             ),
-    .result_out (Thi_muldiv_wire          )
+    .clk        (clk             ),
+    .rst        (rst             ),
+    .x_in       (Sec_a_reg       ),
+    .y_in       (Sec_div_reg     ),
+    .result_out (Thi_muldiv_wire )
 );
 
 Wallace12x12 Wallace12x12_1 (
-    .clk        (clk          ),
-    .rst        (rst          ),
-    .x_in       (Sec_b_reg             ),
-    .y_in       ({Sec_rom_reg[10:0],1'b0}),//Q.12
+    .clk        (clk                      ),
+    .rst        (rst                      ),
+    .x_in       (Sec_b_reg                ),
+    .y_in       ({Sec_rom_reg[10:0],1'b0} ),//Q.12
     .result_out (Thi_bcos_wire            )
 );
 
@@ -213,7 +202,7 @@ end
 wire [23:0] shift_wire;
 assign shift_wire = Thi_muldiv_wire>>Thi_sft_reg;
 
-reg [11:0] Fif_bcos_reg;//(b*cos c)>>12
+reg [11:0] Fif_bcos_reg;
 
 always @(posedge clk) begin
     if (rst) begin
@@ -253,11 +242,11 @@ wire [23:0] Fif_mul_wire;
 reg [11:0]  Fif_mul_reg;
 
 Wallace12x12 Wallace12x12_3 (
-    .clk        (clk          ),
-    .rst        (rst          ),
-    .x_in       (Thi_muldiv_reg       ),//Q.12
-    .y_in       (Thi_bcos_reg    ),//Q.12
-    .result_out (Fif_mul_wire            )//Q.24
+    .clk        (clk            ),
+    .rst        (rst            ),
+    .x_in       (Thi_muldiv_reg ),
+    .y_in       (Thi_bcos_reg   ),
+    .result_out (Fif_mul_wire   )
 );
 
 always @(posedge clk) begin
@@ -289,7 +278,6 @@ always @(posedge clk) begin
     end
 end
 
-
 assign y = {Fif_sign_reg,Fif_mul_reg};
 
 `ifdef SIMULATION
@@ -301,5 +289,3 @@ end
 `endif
 
 endmodule
-
-
